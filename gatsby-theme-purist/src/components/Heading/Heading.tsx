27b70891cf1b7454072016f9622b00472f71b474
Sebastian Ojeda <sebastian.ojeda38@gmail.com>
2021-01-28 23:02:48 -0700

    docs: update theme in README

--- gatsby-theme-purist/src/components/Heading/Heading.tsx
+++ gatsby-theme-purist/src/components/Heading/Heading.tsx
@@ -3,35 +3,35 @@ import styled from '@emotion/styled';
 export const H1 = styled.h1`
   font-size: 2.25rem;
   line-height: 2.5rem;
-  color: var(--color-header);
+  color: var(--color-text-header);
 `;
 
 export const H2 = styled.h2`
   font-size: 1.875rem;
   line-height: 2.25rem;
-  color: var(--color-header);
+  color: var(--color-text-header);
 `;
 
 export const H3 = styled.h3`
   font-size: 1.5rem;
   line-height: 2rem;
-  color: var(--color-header);
+  color: var(--color-text-header);
 `;
 
 export const H4 = styled.h4`
   font-size: 1.25rem;
   line-height: 1.75rem;
-  color: var(--color-header);
+  color: var(--color-text-header);
 `;
 
 export const H5 = styled.h5`
   font-size: 1.25rem;
   line-height: 1.75rem;
-  color: var(--color-header);
+  color: var(--color-text-header);
 `;
 
 export const H6 = styled.h6`
   font-size: 1.25rem;
   line-height: 1.75rem;
-  color: var(--color-header);
+  color: var(--color-text-header);
 `;
